fd8f6e895ff7dd15a2b1c4402fc977b1adf4f9e1
ParthivxCoding779 <81411019+ParthivxCoding779@users.noreply.github.com>
2021-06-21 17:54:56 +0530

    Add files via upload

---
+++ Portfolio/code.js
@@ -0,0 +1,8 @@
+
+function colorchange(){
+    document.getElementById('color1').className = "changingColor";
+}
+function changeBackgroundColor(){
+    document.getElementById("projects").style.background = "rgb(144,169,200)";
+}
+
